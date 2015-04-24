c777f9f98b549ff798e4fb4702b6a7fa43a5dfe2
DeLightNight <asaffisher@icloud.com>
2015-04-24 14:30:43 +0000

    Chenged name of 'photos test' to 'Photos-Arranger'

---
+++ Photos-Arranger/photosAranger.py
@@ -0,0 +1,35 @@
+# your code goes here
+import glob
+import os
+photosArray = glob.glob("*.png")#gets the photos in the imgs fold..
+print photosArray
+if len(photosArray)>=9:#10 photos were created?
+   print "YAY lets start the bubble sorting!"
+   #sort
+   alist = photosArray
+   for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if os.path.getmtime(alist[i])>os.path.getmtime(alist[i+1]):
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+   print photosArray
+   a = 1
+   
+   while(a<5):
+    	os.remove(photosArray[a])
+    	photosArray.remove(photosArray[a])
+    	a=a+1
+   print photosArray
+
+
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if os.path.getmtime(alist[i])>os.path.getmtime(alist[i+1]):
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+
