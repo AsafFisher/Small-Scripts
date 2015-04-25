8307f0ca027a48f9ced22bb76019063d7216c345
DeLightNight <asaffisher@icloud.com>
2015-04-25 14:06:28 +0000

    Added comments to 'photosAranger.py'.

--- Photos-Arranger/photosAranger.py
+++ Photos-Arranger/photosAranger.py
@@ -7,6 +7,7 @@ if len(photosArray)>=9:#10 photos were created?
    print "YAY lets start the bubble sorting!"
    #sort
    alist = photosArray
+   # bubble sorting
    for passnum in range(len(alist)-1,0,-1):
         for i in range(passnum):
             if os.path.getmtime(alist[i])>os.path.getmtime(alist[i+1]):
@@ -16,20 +17,11 @@ if len(photosArray)>=9:#10 photos were created?
 
    print photosArray
    a = 1
-   
+   # remove the oldest 5 files (pics)
    while(a<5):
     	os.remove(photosArray[a])
     	photosArray.remove(photosArray[a])
     	a=a+1
    print photosArray
-
-
-def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
-        for i in range(passnum):
-            if os.path.getmtime(alist[i])>os.path.getmtime(alist[i+1]):
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
 
 
